7b37d0d6533a19f37ac4d1ab64e1906b1fe2c1e1
James Donovan <jameswilliamdonovan@gmail.com>
2020-12-15 22:29:04 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -18,9 +18,9 @@ class Song
   end
 
   def self.new_by_filename(filename)
-    artist, song = filename.split(" - ")
-    new_song = self.new(song)
-    new_song.artist_name = artist
-    new_song
+    artist, song_name = filename.split(" - ")
+    song = Song.new(song_name)
+    song.artist_name = artist
+    song
   end
 end
